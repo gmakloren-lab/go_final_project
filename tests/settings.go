c8b5e49c0b21cbaa3d8efae4b0871d5a1bbd5585
gmakloren-lab <gmakloren@gmail.com>
2026-03-25 14:28:43 +0300

    Добавил Dockerfile, аутентификацию и README

--- tests/settings.go
+++ tests/settings.go
@@ -3,5 +3,5 @@ package tests
 var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = false
-var Search = false
-var Token = ``
+var Search = true
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoYXNoIjoiNTk5NDQ3MWFiYjAxMTEyYWZjYzE4MTU5ZjZjYzc0YjRmNTExYjk5ODA2ZGE1OWIzY2FmNWE5YzE3M2NhY2ZjNSIsImV4cCI6MTc3NDQ1OTQzMn0.thTNfDR_TAl6Bdi9eeOeAMvUnzpDxFOesPufX2o34sc`
